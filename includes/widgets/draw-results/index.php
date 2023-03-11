500c59e7ca1d9453f85ce59f44d4a0fedfcd28f9
unikforceit <freelancerakibul@gmail.com>
2023-03-11 05:46:45 -0600

    winner page done and draw page running

--- includes/widgets/draw-results/index.php
+++ includes/widgets/draw-results/index.php
@@ -32,161 +32,27 @@ class lotteryaddons_draw_res extends Widget_Base
         $this->start_controls_section(
             'content_section',
             [
-                'label' => __('Course Date', 'lotteryaddons'),
+                'label' => __('Draw List', 'lotteryaddons'),
                 'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
             ]
         );
         $this->add_control(
-            'show_cat',
+            'number_winner',
             [
-                'label' => esc_html__('Show Category', 'lotteryaddons'),
+                'label' => esc_html__('Number Of Winner', 'lotteryaddons'),
                 'type' => \Elementor\Controls_Manager::NUMBER,
-                'default' => esc_html__('5', 'lotteryaddons'),
-            ]
-        );
-        $this->end_controls_section();
-
-        $this->start_controls_section(
-            'content_sty',
-            [
-                'label' => __('Style', 'lotteryaddons'),
-                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
+                'default' => 10,
             ]
         );
         $this->add_control(
-            'post_title_color',
+            'show_pagi',
             [
-                'label' => __('Title Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item h4' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'post_titleh_color',
-            [
-                'label' => __('Title Hover Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item h4:hover' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_group_control(
-            \Elementor\Group_Control_Typography::get_type(),
-            [
-                'name' => 'ttih',
-                'label' => __('Title Typography', 'lotteryaddons'),
-                'selector' => '{{WRAPPER}} .service .item h4',
-            ]
-        );
-        $this->add_control(
-            'post_in_color',
-            [
-                'label' => __('Info Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item p' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_group_control(
-            \Elementor\Group_Control_Typography::get_type(),
-            [
-                'name' => 'ttihii',
-                'label' => __('Info Typography', 'lotteryaddons'),
-                'selector' => '{{WRAPPER}} .service .item p',
-            ]
-        );
-        $this->add_control(
-            'icon_c',
-            [
-                'label' => __('Icon Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item .hexagon i' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'icon_cl',
-            [
-                'label' => __('Icon Hover', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item:hover .hexagon i' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'icon_bg',
-            [
-                'label' => __('Icon Hover Background', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item:hover .hexagon:before, 
-                    {{WRAPPER}} .service .item:hover .hexagon:after, {{WRAPPER}} .service .item:hover .hexagon' => 'border-color: {{VALUE}}; background: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_group_control(
-            \Elementor\Group_Control_Typography::get_type(),
-            [
-                'name' => 'btnt',
-                'label' => __('Button Typography', 'lotteryaddons'),
-                'selector' => '{{WRAPPER}} .service .item .thm-btn',
-            ]
-        );
-        $this->add_control(
-            'btn_c',
-            [
-                'label' => __('Button Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item .thm-btn' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'btn_bg',
-            [
-                'label' => __('Button Background', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item .thm-btn' => 'background: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'btn_hc',
-            [
-                'label' => __('Button Hover Color', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item:hover .thm-btn' => 'color: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_control(
-            'btn_bgh',
-            [
-                'label' => __('Button Hover Background', 'lotteryaddons'),
-                'type' => \Elementor\Controls_Manager::COLOR,
-                'selectors' => [
-                    '{{WRAPPER}} .service .item:hover .thm-btn' => 'background: {{VALUE}}',
-                ],
-            ]
-        );
-        $this->add_responsive_control(
-            'content_margin',
-            [
-                'label' => __('Row Gap', 'lotteryaddons'),
-                'type' => Controls_Manager::DIMENSIONS,
-                'size_units' => ['px', '%', 'em'],
-                'selectors' => [
-                    '{{WRAPPER}} .service .item' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-                ],
+                'label' => __( 'Show Pagination', 'lotteryaddons' ),
+                'type' => Controls_Manager::SWITCHER,
+                'default' => 'yes',
+                'label_on' => esc_html__( 'Show', 'lotteryaddons' ),
+                'label_off' => esc_html__( 'Hide', 'lotteryaddons' ),
+                'return_value' => 'yes',
             ]
         );
         $this->end_controls_section();
@@ -196,53 +62,67 @@ class lotteryaddons_draw_res extends Widget_Base
     protected function render()
     {
         $settings = $this->get_settings_for_display();
-        $winners = lty_get_lottery_winners();
+        $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+        $args = array(
+            'post_type' => 'lty_lottery_winner',
+            'post_status' => array('lty_publish'),
+            'posts_per_page' => $settings['number_winner'],
+            'fields' => 'ids',
+            'orderby' => 'ID',
+            'paged' => $paged
+        );
         ?>
-        <div class="draw-lists-lottery-addons">
-            <?php
-            if (!empty($winners)){
-            foreach ($winners as $winner) {
-                $winner = lty_get_lottery_winner($winner);
-                $order_id = $winner->get_order_id();
-                $order = wc_get_order($order_id);
+        <div class="lty-a-competition-results-wrapper">
+        <div class="lty-a-result-wrap">
+        <?php
+        $query = new \WP_Query($args);
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                $winner = lty_get_lottery_winner(get_the_ID());
                 $end_date = \LTY_Date_Time::get_wp_format_datetime_from_gmt( $winner->get_product()->get_lty_end_date_gmt(), false, ' ', false );
                 ?>
-                <div class="draw-result-box-lt-a">
-                    <div class="winner-img-lty-a">
-                        <?php echo wp_kses_post($winner->get_product()->get_image('full')); ?>
-                    </div>
-                    <div class="winner-body-txt">
-                        <h4><?php echo esc_html($winner->get_product()->get_title()); ?></h4>
-                        <p><?php echo esc_html($winner->get_user()->first_name); ?> <?php echo esc_html($winner->get_user()->last_name); ?> from <?php echo esc_html($order->get_billing_city()); ?>
-                            <?php echo esc_html( $end_date ); ?></p>
-                        <span>Ticket #<?php echo esc_html($winner->get_lottery_ticket_number()); ?></span>
+                <div class="lty-a-winner-card">
+                    <div class="lty-a-card-body">
+                        <h4 class="lty-a-card-title"><?php echo esc_html( $end_date ); ?></h4>
+
+                        <div class="lty-a-single-result">
+                            <strong></strong>
+                            <span class="ticket-winner-name"><?php echo esc_html($winner->get_user()->first_name); ?> <?php echo esc_html($winner->get_user()->last_name); ?></span>
+                            <span class="ticket-winner-tn"> - Ticket #<?php echo esc_html($winner->get_lottery_ticket_number()); ?></span>
+                        </div>
+
                     </div>
                 </div>
-            <?php } }?>
+                <?php
+            }
+            // Display pagination links
+            ?>
+        </div>
+            <div class="lty-pagination-wrap">
+            <?php
+            if ($settings['show_pagi']) {
+                // Display pagination links
+                $big = 999999999; // need an unlikely integer
+                echo paginate_links(array(
+                    'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+                    'format' => '?paged=%#%',
+                    'current' => max(1, $paged),
+                    'total' => $query->max_num_pages,
+                    'prev_text' => __('« Prev'),
+                    'next_text' => __('Next »'),
+                    'mid_size' => 4, // number of page links to show on each side of the current page
+                    'type' => 'list', // output pagination links as an unordered list
+                ));
+            }
+            ?>
+            </div>
+            <?php
+        }
+        wp_reset_postdata();
+        ?>
         </div>
         <?php
-        // Custom WP query query
-//        $args_query = array(
-//            'post_type' => 'lty_lottery_winner',
-//            'post_status' => array('lty_publish'),
-//            'posts_per_page' => '-1',
-//            'fields' => 'ids',
-//            'orderby' => 'ID'
-//        );
-//
-//        $query = new \WP_Query( $args_query );
-//
-//        if ( $query->have_posts() ) {
-//            while ( $query->have_posts() ) {
-//                $query->the_post();
-//                the_title();
-//            }
-//        } else {
-//            echo 'No Winners!';
-//        }
-//
-//        wp_reset_postdata();
-//
     }
 
 
